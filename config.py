46b2a1bf0563a717d38feb513a303dcfef0ac246
Vigilans <vigilans@foxmail.com>
2018-05-20 17:16:45 +0800

    Readme与Config更新

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
-mcts_config = {
-    "c_puct": 1.4,
-    "c_iterations": 800
+MCTS_CONFIG = {
+    "c_puct": 5,
+    "c_iterations": 1600,
+    "c_duration": 950
 }
 
-network_config = {
-
+DATA_CONFIG = {
+    # one of "random", "botzone", "default_mcts", "alpha_zero"
+    "agents": [
+        ("botzone", ["./training_data/hdl.exe"]),
+        ("botzone", ["./training_data/deep.exe"])
+    ],
+    "process_num": 2,
+    "buffer_size": 10000,
+    "data_path": "./training_data",
+    "data_files": [
+        # after read out, file name will be marked ".consumed"
+    ]
 }
 
-training_config = {
-
+TRAINING_CONFIG = {
+    "num_epoches": 10,
+    "batch_size": 32,
+    "learning_rate": 0.1,
+    "momentum": 0.9,
+    "eval_period": 128,
+    "model_file": "latest",
+    "model_path": "./trained_models"
 }
--- readme.md
+++ readme.md
@@ -6,20 +6,25 @@
 - [x] MCTS基本结构
 - [x] Cpp版Botzone接口程序
 - [x] 标准化core模块的文件结构
+- [x] 将Core模块进一步封装为Python包
+- [x] 自动搜索Core模块路径
+- [ ] 实现AlphaZero神经网络
 
 ### WIP
 
 - [x] 为Core模块编写测试代码
-- [ ] 自动搜索Python路径以提供env-independent支持
-- [ ] 将Core模块进一步封装为Python包
-- [ ] 序列化蒙特卡洛树
+
+- [ ] 训练AlphaZero网络
+- [x] 更加模块化的Botzone交互接口
 
 ### To-do
-
-- [ ] 更加模块化的Botzone交互接口
-
+暂无。
 
 
+## 版本
+
+* C++ 17
+* Python 3.6
 
 ## 第三方库使用
 
@@ -28,3 +33,12 @@
 * gtest（Adapter先装扩展再装vs_installer)
 * nlohmann::json
 * pybind11
+* Eigen3
+* tensorflow_cc
+
+### Python
+
+* numpy
+* h5py
+* keras
+* tensorflow
