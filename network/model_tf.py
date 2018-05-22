71254ae6f1d2bf7d260dd23aa3032628bbdb15e7
Vigilans <vigilans@foxmail.com>
2018-05-22 16:06:06 +0800

    model_tf多处细节改动： 1. typo及bug修正； 2. Dataset中输入直接以float32格式存储； 3. 输入通道改为默认格式(channels_last)，输入通过一层transpose进行置换； 4. 统一state_value格式（[None, ]） 5. 模型的初始化改为Lazy模式 6. l2_reg只作用于weights（即kernel，剔除了bias变量） 7. early stop时，通过iterator来skip，而非dataset 8. train_step同时返回成功训练的epoch数

--- network/model_tf.py
+++ network/model_tf.py
@@ -1,34 +1,36 @@
 from core import GameConfig, Board
+from config import TRAINING_CONFIG as config
 import tensorflow as tf
 import numpy as np
+import os
 
 
 class PolicyValueNetwork:
-    def __init__(self, model_file=None):
+    def __init__(self, model_name=None):
         with tf.variable_scope("Dataset"):
             input_shape = Board().encoded_states().shape  # (6, 15, 15)
             self.iter = tf.data.Iterator.from_structure(
-                (tf.int8, tf.float32, tf.float32),
+                (tf.float32, tf.float32, tf.float32),
                 (
                     tf.TensorShape((None, *input_shape)),
                     tf.TensorShape((None, )),
-                    tf.TensorShape((None, GameConfig.board_size))
+                    tf.TensorShape((None, GameConfig["board_size"]))
                 )
             )
-            self.inputs, self.state_value, self.policy_output = self.iter.get_next()
+            self.inputs, self.state_value, self.action_probs = self.iter.get_next()
+            inputs_t = tf.transpose(self.inputs, [0, 2, 3, 1])  # channel_last
 
         with tf.variable_scope("SharedNet"):
             conv_blocks = []
             for i in range(3):
                 conv_blocks.append(
                     tf.layers.conv2d(
-                        inputs=self.inputs if i == 0 else conv_blocks[i - 1],
+                        inputs=inputs_t if i == 0 else conv_blocks[i - 1],
                         filters=2**(i + 5),  # 32, 64, 128
                         kernel_size=(3, 3),
                         padding="same",
-                        data_format="channels_first",
                         activation=tf.nn.relu,
-                        name="conv {}".format(i)
+                        name="conv_{}".format(i)
                     )
                 )
             shared_output = conv_blocks[-1]
@@ -39,11 +41,10 @@ class PolicyValueNetwork:
                 filters=4,
                 kernel_size=(1, 1),
                 padding="same",
-                data_format="channels_first",
                 activation=tf.nn.relu
             )
             policy_flatten = tf.layers.flatten(policy_conv)
-            self.policy_logits = tf.layers.dense(policy_flatten, GameConfig.board_size)
+            self.policy_logits = tf.layers.dense(policy_flatten, GameConfig["board_size"])
             self.policy_output = tf.nn.softmax(self.policy_logits)
 
         with tf.variable_scope("ValueHead"):
@@ -52,21 +53,23 @@ class PolicyValueNetwork:
                 filters=2,
                 kernel_size=(1, 1),
                 padding="same",
-                data_format="channels_first",
                 activation=tf.nn.relu
             )
             value_flatten = tf.layers.flatten(value_conv)
             value_hidden = tf.layers.dense(value_flatten, 64, tf.nn.relu)
-            self.value_output = tf.layers.dense(value_hidden, 1, tf.nn.relu)
+            value_logits = tf.layers.dense(value_hidden, 1)
+            self.value_output = tf.reshape(tf.nn.tanh(value_logits), [-1])
 
-        self.session = tf.session()
-        self.session.run(tf.global_variables_initializer())
-        self.saver = tf.train.saver()
+        self.session = tf.Session()
+        self.saver = tf.train.Saver()
 
-        if model_file is not None:
-            self.restore_model(model_file)
+        if model_name is not None:
+            self.model_file = config["model_path"] + "/tf/" + model_name
+        else:
+            self.model_file = None
+        self.initialized = False
 
-    def compile(self, opt):
+    def compile(self):
         """
         Loss/Entropy and Optimization definition
         """
@@ -76,30 +79,32 @@ class PolicyValueNetwork:
             policy_loss = tf.losses.softmax_cross_entropy(self.action_probs, self.policy_logits)
             l2_reg = tf.contrib.layers.apply_regularization(
                 tf.contrib.layers.l2_regularizer(1e-4),
-                tf.trainable_variables()  # TODO: check whether...
+                [v for v in tf.trainable_variables() if "bias" not in v.name]
             )
             # final loss
             self.loss = value_loss + policy_loss + l2_reg
             self.entropy = tf.reduce_mean(
-                -tf.reduce_sum(self.policy_output * tf.log(self.policy_output), 1)
+                -tf.reduce_sum(self.policy_output * tf.log(self.policy_output + 1e-10), 1)
             )
 
         with tf.variable_scope("Optimizer"):
-            self.lr = tf.placeholder(tf.float32, "learning_rate")
+            self.lr = tf.placeholder(tf.float32, name="learning_rate")
             self.opt = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+        self._lazy_initialize()
 
     def build_dataset(self, generator, num_epoches):
         """
         Generator output:
         ([state_batch, value_batch, policy_batch]*num_epoches...)
         """
-        placeholders = [self.input, self.value_output, self.policy_output]
+        placeholders = [self.inputs, self.value_output, self.policy_output]
         self.dataset = tf.data.Dataset.from_generator(
             generator,
-            [data.dtype for data in placeholders],
-            [data.shape for data in placeholders]
+            tuple(data.dtype for data in placeholders),
+            tuple(data.shape for data in placeholders)
         )
-        self.dataset = self.dataset.prefetch(2*num_epoches)
+        self.dataset = self.dataset.prefetch(num_epoches)
         self.session.run(self.iter.make_initializer(self.dataset))
 
     def train_step(self, optimizer, num_epoches):
@@ -110,37 +115,47 @@ class PolicyValueNetwork:
         for i in range(num_epoches):
             new_probs, loss, entropy, _ = self.session.run(
                 [self.policy_output, self.loss, self.entropy, self.opt],
-                feed_dict={self.lr: optimizer["lr"]*optimizer["lr_multiplier"]}
+                feed_dict={self.lr: optimizer["lr"]}
             )
 
             if i == 0:
-                old_probs = new_probs
+                old_probs = new_probs + 1e-10
                 kl = 0  # KL divergence is apparently zero
             else:
-                kl = tf.reduce_mean(
-                    tf.reduce_sum(old_probs * tf.log(old_probs / new_probs), 1)
-                )
+                new_probs += 1e-10
+                kl = (old_probs * np.log(old_probs / new_probs)).sum(1).mean()
 
             # early stop when KL diverges too much
             if kl > 4 * optimizer["kl_target"]:
-                self.dataset = self.dataset.skip(num_epoches - i)
-                print("Early stop and skip {} elems".format(num_epoches - i))
+                for _ in range(num_epoches - 1 - i):  # skip redundant data
+                    self.session.run(self.iter.get_next())
                 break
 
-        return loss, entropy, kl
+        return loss, entropy, kl, i + 1
 
     def eval_state(self, state):
         """
         Evaluate a board state.
         """
+        self._lazy_initialize()
         value, probs = self.session.run(
             [self.value_output, self.policy_output],
             feed_dict={self.inputs: state.encoded_states()[np.newaxis, :]}
         )
-        return value, probs
+        return value[0], probs[0]
 
     def save_model(self, model_path):
         self.saver.save(self.session, model_path)
 
     def restore_model(self, model_path):
         self.saver.restore(self.session, model_path)
+        self.initialized = True
+
+    def _lazy_initialize(self):
+        if self.initialized:
+            return
+        if not self.model_file or not os.path.exists(self.model_file):
+            self.session.run(tf.global_variables_initializer())
+            self.initialized = True
+        else:
+            self.restore_model(self.model_file)
