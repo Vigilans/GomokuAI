7a79c8344a0228c0804d2c4ebb5d8b98007c6876
Vigilans <vigilans@foxmail.com>
2018-05-23 08:51:56 +0800

    添加human_agent

---
+++ agents/human_agent.py
@@ -0,0 +1,36 @@
+if __name__ == "__main__":
+    from __init__ import Agent
+else:
+    from .agent import Agent
+
+from core import Position, GameConfig
+import numpy as np
+
+
+class ConsoleAgent(Agent):
+    def __init__(self):
+        pass
+
+    def get_action(self, state):
+        action = input("Input your move: ")
+        for separator in [' ', ',']:
+            if separator in action:
+                return Position(map(int, action.split(separator)))
+
+    def eval_state(self, state):
+        action = input("Input your move: ")
+        state_value = input("Input your state value estimation: ")
+        for separator in [' ', ',']:
+            if separator in action:
+                action = Position(map(int, action.split(separator)))
+                action_probs = np.zeros(GameConfig["board_size"])
+                action_probs[int(action)] = 1.0
+                return state_value, action_probs, action
+
+    def __repr__(self):
+        return "Human Console Agent"
+
+
+if __name__ == "__main__":
+    from utils import botzone_interface
+    botzone_interface(ConsoleAgent())
